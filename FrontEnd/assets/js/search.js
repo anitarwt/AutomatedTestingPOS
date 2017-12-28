00cb2f5aff196e76b0a0656ef924709d6587c258
DESKTOP-JTVA7CE\fredy <fredy.setiawan@students.amikom.ac.id>
2017-12-28 14:02:53 +0700

    implement search js

--- FrontEnd/assets/js/search.js
+++ FrontEnd/assets/js/search.js
@@ -1,7 +1,10 @@
-var search = angular.module("search",[]);
-employeeApp.controller("searchCtrl",function($scope,$http){
+angular.module("search",[]).controller("searchCtrl",function($scope,$http){
     $http.get("http://localhost:8000/api/tempatmakan")
     .then(function(response) {
         $scope.tempatmakan = response.data;
     });               
+});
+
+angular.module("carilokasi",[]).controller("lokasisendiri",function($scope,$http){
+    
 });
